ccaa3faa9188a2e1113c4c7c8a8eaf72e50c0e82
imannfazal <imannfazal@gmail.com>
2024-10-01 17:33:42 +0500

    fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 //import Gallery from "./components/Gallery";
 import Header2 from "./components/Header2";
 import Hero from "./components/Hero";
+import "./App.css";
+import "./fonts/Krub-Regular.ttf";
+import "./fonts/ClassicalDiary-Regular Demo.ttf"
 
 function App() {
   return (
-    <div className="p-30 bg-gradient-to-r from-[#7F7070] to-[#380733] w-full" >
+    <div className="fontKrub p-30 bg-gradient-to-r from-[#7F7070] to-[#380733] w-full" >
       <Header2 />
       <Hero />
     </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import Gallery from './Gallery'
 const Hero = () => {
   return (
     <div class='ml-[70px] mt-28 text-[#ffffff]'>
-        <div class='text-[33px]'>Hello, it’s Iman.</div>
+        <div class='text-[33px] fontClassical'>Hello, it’s Iman.</div>
         <div class='text-[25px] mt-4 '>If you’re looking for a freelance designer to help bring an idea to life and you’re on tight timeline-let’s jam.</div>
         <div class='text-[25px] mt-12'>Work Gallery</div>
         <Gallery />
