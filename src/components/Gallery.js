a55089a5b862e716fc4529cf60205de52826516e
imannfazal <imannfazal@gmail.com>
2024-10-02 14:02:51 +0500

    border set

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -11,13 +11,13 @@ const Gallery = () => {
     return (
         <div>
             <div class="inline-flex mt-[20px] text-[#5f5f5f] text-[20px]">
-                <img class='lift cursor-pointer h-[300px] w-[390px]' alt='work' src={Daily} />
-                <img class='lift cursor-pointer ml-24 h-[300px] w-[390px]' alt='work' src={SRE} />
-                <img class='lift cursor-pointer ml-24 h-[300px] w-[390px]' alt='work' src={dex} />
+                <img class='lift cursor-pointer h-[300px] w-[390px] rounded-[5px]' alt='work' src={Daily} />
+                <img class='lift cursor-pointer ml-24 h-[300px] w-[390px] rounded-[5px]' alt='work' src={SRE} />
+                <img class='lift cursor-pointer ml-24 h-[300px] w-[390px] rounded-[5px]' alt='work' src={dex} />
             </div>
             <div class="inline-flex mt-[50px] text-[#5f5f5f] text-[20px]">
-                <img class='h-[300px] w-[390px] lift cursor-pointer' alt='work' src={Rush} />
-                <img class='h-[300px] w-[390px] lift cursor-pointer ml-24' width='100%' alt='work' src={bank} />
+                <img class='h-[300px] w-[390px] lift cursor-pointer rounded-[5px]' alt='work' src={Rush} />
+                <img class='h-[300px] w-[390px] lift cursor-pointer ml-24 rounded-[5px]' width='100%' alt='work' src={bank} />
                 {/* <img class='h-[300px] w-[390px] lift cursor-pointer ml-24' alt='work' src={img} /> */}
             </div>
         </div>
