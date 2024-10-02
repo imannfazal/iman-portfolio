f3754ffe7b0f1ed80e02f8e580350819f05631ab
imannfazal <imannfazal@gmail.com>
2024-10-02 15:59:44 +0500

    added names

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+const Image = (props) => (
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="h-[320px] w-[390px]" src={props.src} alt="Sunset in the mountains" />
+        <div class="px-6 py-4">
+            <div class="text-[15px] text-[#ffffff] mb-2">{props.text}</div>
+        </div>
+    </div>
+);
+export default Image;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import img from '../assets/work.png'
 import './Gallery.css'
 import SRE from '../assets/SRE.png'
@@ -6,19 +6,24 @@ import Daily from '../assets/daily.png'
 import Rush from '../assets/rush.png'
 import dex from '../assets/dex.png'
 import bank from '../assets/bank.png'
+import Card from './Card'
+import Image from './Card'
 
 const Gallery = () => {
     return (
         <div>
             <div class="inline-flex mt-[20px] text-[#5f5f5f] text-[20px]">
-                <img class='lift cursor-pointer h-[300px] w-[390px] rounded-[5px]' alt='work' src={Daily} />
-                <img class='lift cursor-pointer ml-24 h-[300px] w-[390px] rounded-[5px]' alt='work' src={SRE} />
-                <img class='lift cursor-pointer ml-24 h-[300px] w-[390px] rounded-[5px]' alt='work' src={dex} />
+                <div class='lift cursor-pointer'><Image text='indexx Fortune Daily' src={Daily} /></div>
+                <div class='ml-24 lift cursor-pointer'><Image text='Discipline Planning System' src={SRE} /></div>
+                <div class='ml-24 lift cursor-pointer'><Image text='indexx Bank' src={bank} /></div>
+                {/* <img class='lift cursor-pointer h-[300px] w-[390px] rounded-[5px]' alt='work' src={Daily} /> */}
+                {/* <img class='lift cursor-pointer ml-24 h-[300px] w-[390px] rounded-[5px]' alt='work' src={SRE} />
+                <img class='lift cursor-pointer ml-24 h-[300px] w-[390px] rounded-[5px] ' alt='work' src={dex} /> */}
             </div>
             <div class="inline-flex mt-[50px] text-[#5f5f5f] text-[20px]">
-                <img class='h-[300px] w-[390px] lift cursor-pointer rounded-[5px]' alt='work' src={Rush} />
-                <img class='h-[300px] w-[390px] lift cursor-pointer ml-24 rounded-[5px]' width='100%' alt='work' src={bank} />
-                {/* <img class='h-[300px] w-[390px] lift cursor-pointer ml-24' alt='work' src={img} /> */}
+            <div class='lift cursor-pointer'><Image text='indexx Decentralized Exchange' src={dex} /></div>
+            <div class='ml-24 lift cursor-pointer'><Image text='indexx Rush' src={Rush} /></div>
+            {/* <img class='h-[300px] w-[390px] lift cursor-pointer ml-24' alt='work' src={img} /> */}
             </div>
         </div>
     )
