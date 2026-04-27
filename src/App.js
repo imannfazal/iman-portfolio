c577eb97a22bed87cbda027c49644cb3a2348990
imannfazal <imannfazal@gmail.com>
2026-04-27 08:25:44 +0400

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./fonts/Krub-Regular.ttf";
 import "./fonts/ClassicalDiary-Regular Demo.ttf"
